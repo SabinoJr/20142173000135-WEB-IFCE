ff81e2cb07b1f3c012ba47854a392f9c419f6857
unknown <sabinogoncalvesdasilvajunior@gmail.com>
2018-04-20 23:12:05 -0300

    Lista de atividade JavaScript

---
+++ Projeto de Sistemas WEB/www/Atividades/Atividade_14_Lista/Exercicio_1/script.js
@@ -0,0 +1,8 @@
+function mostraMensagem()
+{
+	alert('AIA!');
+	
+}
+
+
+
---
+++ Projeto de Sistemas WEB/www/Atividades/Atividade_14_Lista/Exercicio_3/exercicio3.js
@@ -0,0 +1,5 @@
+document.getElementById("myBtn").addEventListener("click", abrir);
+function abrir()
+{
+window.open('pagina.html','_blank');
+}
